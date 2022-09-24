b00b5a5e9c8600d0667f4d51b1ef5ae6c7cb72dd
Danmar Varela <vareladanmar@gmail.com>
2022-09-24 12:56:04 +0800

    Add slack logo asset

--- src/Logo.js
+++ src/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Image, keyframes, usePrefersReducedMotion} from "@chakra-ui/react";
-import logo from "./logo.svg";
+import slack from "./assets/slack.svg";
 
 const spin = keyframes`
   from { transform: rotate(0deg); }
@@ -12,5 +12,5 @@ export const Logo = (props) => {
 
   const animation = prefersReducedMotion ? undefined : `${spin} infinite 20s linear`;
 
-  return <Image animation={animation} src={logo} {...props} />;
+  return <Image animation={animation} src={slack} {...props} />;
 };
