662c8a37ae5e98beaf464bc9aca25bac9e57b4f0
Danmar Varela <vareladanmar@gmail.com>
2022-09-28 22:26:01 +0800

    Add router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {ChakraProvider} from "@chakra-ui/react";
+import router from "router";
+import {RouterProvider} from "react-router-dom";
+import theme from "theme";
+
+const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
