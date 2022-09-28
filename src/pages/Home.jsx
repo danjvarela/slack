7ee988cc83b3a0b41c6f4106dd7f6d69138bb694
Danmar Varela <vareladanmar@gmail.com>
2022-09-29 00:04:05 +0800

    Customize Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import {Center, VStack, Text} from "@chakra-ui/react";
+import {Center, VStack, Text, HStack, Heading} from "@chakra-ui/react";
 import {ColorModeSwitcher} from "components/ColorModeSwitcher";
 import Header from "layouts/Header";
-import Card from "components/Card";
+import {ReactComponent as SlackLogo} from "assets/slack.svg";
 
 const Home = () => {
   return (
@@ -10,9 +10,17 @@ const Home = () => {
         <ColorModeSwitcher />
       </Header>
       <Center w="full" flexGrow={1}>
-        <Card>
-          <Text>Somecontent</Text>
-        </Card>
+        <HStack>
+          <VStack alignItems="flex-start">
+            <Heading display="flex">
+              <SlackLogo />
+              Welcome to Slack!
+            </Heading>
+            <Text>
+              Log in now and start collaborating or hanging out with your friends!
+            </Text>
+          </VStack>
+        </HStack>
       </Center>
     </VStack>
   );
