efe45275ed0e18ea8a1fafe19bca6b0fd1d0c4a6
Danmar Varela <vareladanmar@gmail.com>
2022-09-29 23:00:27 +0800

    Enable sending messages

---
+++ src/features/messages/components/MessageBox.jsx
@@ -0,0 +1,41 @@
+import {FormControl, Textarea, IconButton, HStack} from "@chakra-ui/react";
+import {Formik, Form, Field} from "formik";
+import * as Yup from "yup";
+import {MdSend} from "react-icons/md";
+import {postRequest} from "lib/axios";
+import {useAuth} from "context/AuthContextProvider";
+
+const sendMessage = async (values, opts = {}) => {
+  const response = await postRequest("/api/v1/messages", values, opts);
+  console.log(response);
+};
+
+const MessageBox = () => {
+  const {auth} = useAuth();
+  return (
+    <Formik
+      initialValues={{body: ""}}
+      validationSchema={Yup.object({
+        body: Yup.string().required(),
+      })}
+      onSubmit={async (values) => {
+        const completeBody = {...values, receiver_id: "3", receiver_class: "User"};
+        await sendMessage(completeBody, {headers: auth.headers});
+      }}
+    >
+      <HStack as={Form} w="full">
+        <FormControl>
+          <Field
+            as={Textarea}
+            variant="filled"
+            placeholder="Message user 1"
+            name="body"
+          />
+        </FormControl>
+        <IconButton icon={<MdSend />} colorScheme="green" type="submit" />
+      </HStack>
+    </Formik>
+  );
+};
+
+export default MessageBox;
