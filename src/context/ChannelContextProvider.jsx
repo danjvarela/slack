d10bc9a2797b6d18e54529ca5ec323806e36a732
Danmar Varela <vareladanmar@gmail.com>
2022-09-30 10:42:57 +0800

    Add ChannelContextProvider functions

--- src/context/ChannelContextProvider.jsx
+++ src/context/ChannelContextProvider.jsx
@@ -1,6 +1,7 @@
-import {createContext, useMemo, useContext, useState, useEffect} from "react";
+import {createContext, useContext, useState, useEffect} from "react";
 import {useAuth} from "context/AuthContextProvider";
 import {getRequest, postRequest} from "lib/axios";
+import {isEmpty} from "utils";
 
 const ChannelContext = createContext();
 
@@ -8,21 +9,34 @@ const useChannels = () => useContext(ChannelContext);
 
 const ChannelContextProvider = ({children}) => {
   const {auth} = useAuth();
-  const [channels, setChannels] = useState({});
   const [errors, setErrors] = useState([]);
+  const [channels, setChannels] = useState([]);
 
   const getChannels = async () => {
-    const {headers} = auth;
-    const response = await getRequest("/api/v1/channels", {headers});
-    if (response.data?.errors) return setChannels([]);
-    setChannels(response.data.data);
+    const response = await getRequest("/api/v1/channels", {headers: auth.headers});
+    if (!isEmpty(response.data.errors)) return setErrors(response.data.errors);
+    setChannels(response.data.data ?? []);
   };
 
-  const addChannel = async (values, opts = {}) => {
-    const response = await postRequest("/api/v1/channels", values, opts);
-    if (response.data?.errors) return setErrors(response.data.errors);
-    setChannels([...channels, response.data.data]);
-    return response.data.data;
+  const createChannel = async (body) => {
+    const response = await postRequest("/api/v1/channels", {
+      body: body,
+      headers: auth.headers,
+    });
+    if (!isEmpty(response.data.errors)) return setErrors(response.data.errors);
+    setChannels([...channels, response.data.data ?? []]);
+  };
+
+  const getChannelDetails = async (id) => {
+    const response = await getRequest("/api/v1/channels", {params: {id: id}});
+    if (!isEmpty(response.data.errors)) return response.data.errors;
+    return response.data.data ?? {};
+  };
+
+  const addMemberToChannel = async (body) => {
+    const response = await postRequest("/api/v1/channel/add_member", {body: body});
+    if (!isEmpty(response.data.errors)) return response.data.errors;
+    return response.data.data ?? {};
   };
 
   useEffect(() => {
@@ -30,7 +44,16 @@ const ChannelContextProvider = ({children}) => {
   }, [auth]);
 
   return (
-    <ChannelContext.Provider value={{channels, getChannels, addChannel, errors}}>
+    <ChannelContext.Provider
+      value={{
+        channels,
+        errors,
+        getChannels,
+        createChannel,
+        getChannelDetails,
+        addMemberToChannel,
+      }}
+    >
       {children}
     </ChannelContext.Provider>
   );
