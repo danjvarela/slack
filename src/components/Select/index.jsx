fe62377dda65d45f88e6e49d402bae877fdfafdc
Danmar Varela <vareladanmar@gmail.com>
2022-10-03 01:52:05 +0800

    Handle clearing select

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,13 +1,18 @@
 import {FormControl, FormErrorMessage, FormLabel} from "@chakra-ui/react";
 import {AsyncSelect, createFilter} from "chakra-react-select";
 import {useField} from "formik";
+import {isEmpty} from "utils";
 import MenuList from "./MenuList";
 
 const Select = ({label, ...props}) => {
   const [_, meta, {setValue}] = useField(props);
 
   const handleChange = (value) => {
-    setValue(value);
+    if (props.isMulti) {
+      setValue(isEmpty(value) ? [] : value);
+      return;
+    }
+    setValue(isEmpty(value) ? {} : value);
   };
 
   return (
@@ -18,6 +23,8 @@ const Select = ({label, ...props}) => {
         onChange={handleChange}
         filterOption={createFilter({ignoreAccents: false})}
         components={{MenuList}}
+        backspaceRemovesValue
+        isClearable
       />
       {meta.touched && meta.error ? (
         <FormErrorMessage>{meta.error}</FormErrorMessage>
