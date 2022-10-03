44f4b80dd402573bf97db38c9ef8ab75b40bb431
Danmar Varela <vareladanmar@gmail.com>
2022-10-04 02:46:24 +0800

    Add Avatars

---
+++ src/features/channels/components/ChannelMemberAvatars.jsx
@@ -0,0 +1,34 @@
+import {Avatar, AvatarGroup} from "@chakra-ui/react";
+import {useChannels} from "context/ChannelContextProvider";
+import {useUsers} from "context/UserContextProvider";
+import {useEffect, useState} from "react";
+import {isEmpty} from "utils";
+
+const ChannelMemberAvatars = ({channelId}) => {
+  const {getChannelDetails} = useChannels();
+  const {users} = useUsers();
+  const [channelMembers, setChannelMembers] = useState([]);
+
+  useEffect(() => {
+    getChannelDetails(channelId)
+      .then((channel) => channel.channel_members)
+      .then((members) =>
+        members
+          .map((member) => member.id)
+          .map((id) => users.find((user) => user.id === id))
+      )
+      .then((members) => setChannelMembers(members));
+  }, [channelId]);
+
+  return (
+    <AvatarGroup size="xs">
+      {!isEmpty(channelMembers)
+        ? channelMembers.map((member) => (
+            <Avatar name={member?.email ?? ""} key={member?.id} />
+          ))
+        : null}
+    </AvatarGroup>
+  );
+};
+
+export default ChannelMemberAvatars;
--- src/features/messages/components/MessagesHeader.jsx
+++ src/features/messages/components/MessagesHeader.jsx
@@ -1,17 +1,19 @@
-import {Heading, Text} from "@chakra-ui/react";
+import {Heading} from "@chakra-ui/react";
 import Header from "layouts/Header";
 import {useReceivers} from "context/ReceiverContextProvider";
+import ChannelMemberAvatars from "features/channels/components/ChannelMemberAvatars";
 
 const MessagesHeader = () => {
-  const {currentReceiver} = useReceivers();
+  const {currentReceiver: receiver} = useReceivers();
 
-  const title =
-    currentReceiver.class === "Channel" ? currentReceiver.name : currentReceiver.email;
+  const title = receiver.class === "Channel" ? receiver.name : receiver.email;
 
   return (
     <Header size="sm" variant="outlined" justifyContent="space-between">
       <Heading size="sm">{title}</Heading>
-      <Text>Avatars</Text>
+      {receiver.class === "Channel" ? (
+        <ChannelMemberAvatars channelId={receiver.id} />
+      ) : null}
     </Header>
   );
 };
